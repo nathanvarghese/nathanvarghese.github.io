519c9ce45cfa4d2d56215ad8276861af458bb73f
nathanvarghese <144954684+nathanvarghese@users.noreply.github.com>
2025-12-15 05:26:52 +0000

    Deploying to gh-pages from @ nathanvarghese/nathanvarghese.github.io@8e7fdd01f919d39c4cc848316bb3cff47fccb498 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "My GitHub repo highlights!",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
