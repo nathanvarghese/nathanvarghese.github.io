bbf12cd6dc5d286791b8fe0799d89eb96a20ef3a
nathanvarghese <144954684+nathanvarghese@users.noreply.github.com>
2025-12-15 05:46:58 +0000

    Deploying to gh-pages from @ nathanvarghese/nathanvarghese.github.io@2fe6eaa74973293d1e5cacc2ad5180dbca30b9f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This page contains a brief overview of my contact details and education. Please click the link for the latest version of my cv!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
