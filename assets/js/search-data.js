175eb2d86382fc0f5c0609b18f78c82333a1c4f0
nathanvarghese <144954684+nathanvarghese@users.noreply.github.com>
2025-12-16 05:17:35 +0000

    Deploying to gh-pages from @ nathanvarghese/nathanvarghese.github.io@f80013a9a739eb65e643533304f2fd53d488df4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,23 +30,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/my_music/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
+        },{id: "news-i-started-my-internship-at-inspector-bots-sparkles",
+          title: 'I started my internship at Inspector Bots! :sparkles:',
           description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-new-song-released",
+          title: 'New song released!',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-ros-development-kit",
+              window.location.href = "/news/announcement_3.html";
+            },},{id: "projects-ros-development-kit",
           title: 'ROS Development Kit',
           description: "Robotics Software Engineering Intern @ Inspector Bots LLC",
           section: "Projects",handler: () => {
@@ -61,36 +53,6 @@ ninja.data = [{
           description: "Final project of CSCI 5722 - Computer Vision",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-email',
         title: 'email',
